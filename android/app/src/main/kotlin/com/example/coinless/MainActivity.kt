dec172b96703756a44358b91a0673df550287736
taufiqalfianto <taufikalfianto8@students.amikom.ac.id>
2023-12-04 01:04:15 +0700

    feat: frist project, add theme

---
+++ android/app/src/main/kotlin/com/example/coinless/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.coinless
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color bluecolor = const Color(0x00538dff);
+Color darkbluecolor = const Color(0x00427FF7);
+Color whitecolor = const Color(0x00ffffff);
+Color blackcolor = const Color(0x00000000);
+Color greycolor = const Color(0x00f2f2f2);
+Color darkgreycolor = const Color(0x00D9D9D9);
+Color greencolor = const Color(0x0031C791);
+Color redcolor = const Color(0x00FF0000);
+Color yellowcolor = const Color(0x008A5300);
+
+
+
+TextStyle blueTextStyle = GoogleFonts.poppins(
+  color: bluecolor,
+);
+TextStyle darkblueTextStyle = GoogleFonts.poppins(
+  color: darkbluecolor,
+);
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: whitecolor,
+);
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: blackcolor,
+);
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: greycolor,
+);
+TextStyle darkgreyTextStyle = GoogleFonts.poppins(
+  color: darkgreycolor,
+);
+TextStyle greenTextStyle = GoogleFonts.poppins(
+  color: greencolor,
+);
+TextStyle redTextStyle = GoogleFonts.poppins(
+  color: redcolor,
+);
+TextStyle yellowTextStyle = GoogleFonts.poppins(
+  color: yellowcolor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w800;
+FontWeight bold = FontWeight.w900;
